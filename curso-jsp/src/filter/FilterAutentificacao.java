b77f2e078202298112555d2f73c86f73105b3af4
unknown <otavio.mlemos20@gmail.com>
2021-09-01 23:15:31 -0300

    seventh commit
    Foi criado uma tela para mostrar Erros. "erro.jsp"

--- curso-jsp/src/filter/FilterAutentificacao.java
+++ curso-jsp/src/filter/FilterAutentificacao.java
@@ -16,80 +16,75 @@ import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
-@WebFilter(urlPatterns = { "/principal/*" }) /* Intercepta todas as requisições que vierem do projeto ou mapeamento */
+@WebFilter(urlPatterns = {"/principal/*"})/*Interceptas todas as requisiçoes que vierem do projeto ou mapeamento*/
 public class FilterAutentificacao implements Filter {
-	
 	
 	
 	private static Connection connection;
 	
 
-	public FilterAutentificacao() {
+    public FilterAutentificacao() {
+    }
 
-	}
-
-	/* Encerra os processos quando o servidor for parado */
-	/* Mataria o processo de conexão com o banco de dados */
+    /*Encerra os processo quando o servidor é parado*/
+    /*Mataria os processo de conexão com banco*/
 	public void destroy() {
-        try {
+		try {
 			connection.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
 
-	/*
-	 * Intercepta as requisições e as respostas no sistema Tudo no sistema passa por
-	 * aqui Ex: Validação de autentificação Dar commit e rolback de transações no
-	 * banco Validar e fazer redirecionamento de páginas
-	 */
-
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+	/*Intercepta as requisicoes e a as respostas no sistema*/
+	/*Tudo que fizer no sistema vai fazer por aqui*/
+	/*Validação de autenticao*/
+	/*Dar commit e rolback de transaçoes do banco*/
+	/*Validar e fazer redirecionamento de paginas*/
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) 
 			throws IOException, ServletException {
-		
-        try {
+	    try { 
 			HttpServletRequest req = (HttpServletRequest) request;
-	
 			HttpSession session = req.getSession();
-	
+			
 			String usuarioLogado = (String) session.getAttribute("usuario");
-	
-			String urlParaAutenticar = req.getServletPath(); // URL que está sendo acessada
-	
-			// Validar se está logado, se não redireciona para a tela de login
-	
-			if (usuarioLogado == null
-					&& !urlParaAutenticar.equalsIgnoreCase("/principal/ServletLogin")) {/* Não está logado */
-	
+			
+			String urlParaAutenticar = req.getServletPath();/*Url que está sendo acessada*/
+			
+			/*Validar se está logado senão redireciona para a tela de login*/
+			if (usuarioLogado == null  && 
+					!urlParaAutenticar.equalsIgnoreCase("/principal/ServletLogin")) {/*Não está logado*/
+				
 				RequestDispatcher redireciona = request.getRequestDispatcher("/index.jsp?url=" + urlParaAutenticar);
-	
-				request.setAttribute("msg", "Por favor realize o login");
-	
+				request.setAttribute("msg", "Por favor realize o login!");
 				redireciona.forward(request, response);
-	
-				return; // Para a execução e redireciona para o login
-	
-			} else {
+				return; /*Para a execução e redireciona para o login*/
+				
+			}else {
 				chain.doFilter(request, response);
 			}
-        
-			connection.commit();//Se deu tudo certo commita as alterações no banco de dados
 			
-        }catch (Exception e) {
+			connection.commit();/*Deu tudo certo, então comita as alteracoes no banco de dados*/
+		
+	    }catch (Exception e) {
 			e.printStackTrace();
+			
+			RequestDispatcher redirecionar = request.getRequestDispatcher("erro.jsp");
+			request.setAttribute("msg", e.getMessage());
+			redirecionar.forward(request, response);
+			
 			try {
 				connection.rollback();
 			} catch (SQLException e1) {
-				
 				e1.printStackTrace();
 			}
 		}
 	}
 
-	/* Inicia os processos ou recursos quando o servidor sobe o projeto */
-	// Inicia conexão com o banco de dados
+	/*Inicia os processo ou recursos quando o servidor sobre o projeto*/
+	// inicar a conexão com o banco
 	public void init(FilterConfig fConfig) throws ServletException {
-        connection = SingleConnectionBanco.getConnection();
+		connection = SingleConnectionBanco.getConnection();
 	}
 
 }
