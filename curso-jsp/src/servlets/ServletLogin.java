6468c876bb1180e7fa3fad9fc172f545d1296c2b
unknown <otavio.mlemos20@gmail.com>
2021-09-01 22:51:50 -0300

    sixth commit
    Criando DAOLogin e método de autenticação no banco de dados

--- curso-jsp/src/servlets/ServletLogin.java
+++ curso-jsp/src/servlets/ServletLogin.java
@@ -2,6 +2,7 @@ package servlets;
 
 import java.io.IOException;
 
+import dao.DAOLoginRepository;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -10,56 +11,68 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import model.ModelLogin;
 
-@WebServlet(urlPatterns = {"/principal/ServletLogin", "/ServletLogin"}) /* Mapeamento de URL que vem da tela */
+
+/*O chamando Controller são as servlets ou ServletLoginController*/
+@WebServlet(urlPatterns = {"/principal/ServletLogin", "/ServletLogin"}) /*Mapeamento de URL que vem da tela*/
 public class ServletLogin extends HttpServlet {
-
+	
 	private static final long serialVersionUID = 1L;
+	
+	private DAOLoginRepository daoLoginRepository = new DAOLoginRepository();
 
-	public ServletLogin() {
+  
+    public ServletLogin() {
+    }
+
+
+    /*Recebe os dados pela url em parametros*/
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
 	}
 
-	/* Recebe os dados pela url em parametros */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-
-	}
-
-	/* recebe os dados enviados por um formulario */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-
+	
+	/*recebe os dados enviados por um formulario*/
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		String login = request.getParameter("login");
 		String senha = request.getParameter("senha");
 		String url = request.getParameter("url");
-
-		if (login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) {
-
-			ModelLogin modellogin = new ModelLogin();
-			modellogin.setLogin(login);
-			modellogin.setSenha(senha);
-
-			if (modellogin.getLogin().equalsIgnoreCase("admin")
-					&& modellogin.getSenha().equalsIgnoreCase("admin")) {/* Simulando login */
-
-				request.getSession().setAttribute("usuario", modellogin.getLogin());
-				
-				if(url == null || url.equals("null")) {
-					url = "principal/principal.jsp";
+		
+		try {
+		
+				if (login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) {
+					
+					ModelLogin modelLogin = new ModelLogin();
+					modelLogin.setLogin(login);
+					modelLogin.setSenha(senha);
+					
+					if (daoLoginRepository.validarAutenticacao(modelLogin)) { /*Simulando login*/
+						
+						request.getSession().setAttribute("usuario", modelLogin.getLogin());
+						
+						if (url == null || url.equals("null")) {
+							url = "principal/principal.jsp";
+						}
+						
+						RequestDispatcher redirecionar = request.getRequestDispatcher(url);
+						redirecionar.forward(request, response);
+						
+					}else {
+						RequestDispatcher redirecionar = request.getRequestDispatcher("/index.jsp");
+						request.setAttribute("msg", "Informe o login e senha corretamente!");
+						redirecionar.forward(request, response);
+					}
+					
+				}else {
+					RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+					request.setAttribute("msg", "Informe o login e senha corretamente!");
+					redirecionar.forward(request, response);
 				}
-
-				RequestDispatcher redirecionar = request.getRequestDispatcher(url);
-				redirecionar.forward(request, response);
-
-			} else {
-				RequestDispatcher redirecionar = request.getRequestDispatcher("/index.jsp");
-				request.setAttribute("msg", "Informe o login e senha corretamente");
-				redirecionar.forward(request, response);
-			}
-		} else {
-			RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
-			request.setAttribute("msg", "Informe o login e senha corretamente");
-			redirecionar.forward(request, response);
+		
+		}catch (Exception e) {
+			e.printStackTrace();
 		}
-
+		
 	}
+
 }
